da2a061a9c65a06a6872127c6a046b6a0d4b698e
Thiago Marinho <tgmarinho@gmail.com>
2018-01-12 14:21:22 -0300

    adicionando o albumlist no layout

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,7 +2,7 @@
 import SpotifyWrapper from 'spotify-wrapper-wj';
 
 const spotify = new SpotifyWrapper({
-  token: 'API_TOKEN'
+  token: 'BQAb3EXUN91-UTypTej6S_gzUikIdVzJUUWXISibxOXN3mvq4GXkgkvx6cianc_iLq3CCbJ_hfUM-atTFQHrLxrn-Ijro24sYrlRi00uXHJV9VfoY_3vq8asGeYNOYzY-cIjdaSFt9bJL-oo7i0U7v_Mbh1K5fg9tw'
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Spotfy Player Web');
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albums = spotify.search.albums('Diante do Trono');
+const albumList = document.getElementById('album-list');
+
+albums
+    .then(data => renderAlbums(data.albums.items, albumList));
