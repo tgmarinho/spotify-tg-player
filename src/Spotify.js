e54526f51a83a68d46faf79af45354c8d3408c0f
Thiago Marinho <tgmarinho@gmail.com>
2018-01-12 13:39:31 -0300

    importando a biblioteca do spotify wrapper wj

---
+++ src/Spotify.js
@@ -0,0 +1,8 @@
+// to import a specific method
+import SpotifyWrapper from 'spotify-wrapper-wj';
+
+const spotify = new SpotifyWrapper({
+  token: 'API_TOKEN'
+});
+
+export default spotify;
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,17 @@
 import { expect } from 'chai';
+import spotify from '../src/Spotify';
 
-describe('Main', () => {
 
+describe('Spotify TG', () => {
+    it('should be an object', () => {
+        expect(spotify).to.be.a.object;
+    });
+    
+    it('should have searh methods', () => {
+        expect(spotify.search).to.be.exist;
+    });
+
+    it('should have album methods', () => {
+        expect(spotify.album).to.be.exist;
+    });
 });
