67cd170d7d9fc6ce2ea628b21fd571b3905e7b55
= <tgmarinho@gmail.com>
2018-01-13 09:54:00 -0300

    Add albuminfo no layout

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,7 +2,7 @@
 import SpotifyWrapper from 'spotify-wrapper-wj';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQAb3EXUN91-UTypTej6S_gzUikIdVzJUUWXISibxOXN3mvq4GXkgkvx6cianc_iLq3CCbJ_hfUM-atTFQHrLxrn-Ijro24sYrlRi00uXHJV9VfoY_3vq8asGeYNOYzY-cIjdaSFt9bJL-oo7i0U7v_Mbh1K5fg9tw'
+  token: 'BQAVTI-aikPHJyyboLfoPqILuBuZeGIOEzQXMaNnaVcYi0sAFfEuWAaGj3xkXsSGo35SPL7QSu72MuVz27oD6xfGOLtbr8e-pb9D3kBaMAgSWq3egPn0YxosckihbGPPFVItuHI9Lysmic2nWhxaRXTfP09RL0Jn3Q'
 });
 
 export default spotify;
