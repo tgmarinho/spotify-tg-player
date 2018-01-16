4f00eed69feac3ea07a2f440863dd17f1b2abeec
= <tgmarinho@gmail.com>
2018-01-16 12:26:09 -0300

    solved conflict

--- tests/AlbumTracks.spec.js
+++ tests/AlbumTracks.spec.js
@@ -2,10 +2,7 @@ import 'jsdom-global/register';
 import { expect } from 'chai';
 
 import renderAlbumTracks from '../src/AlbumTracks';
-<<<<<<< HEAD
 import convertToHumanTime from '../src/ConvertToHumanTime';
-=======
->>>>>>> d151bb1782b5ff2da6fa71d4250ac3a0c9489528
 
 describe('AlbumTracks', () => {
   const data = [
@@ -34,20 +31,13 @@ describe('AlbumTracks', () => {
 
   const markup = `
     <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4">
-<<<<<<< HEAD
     <p class="music-number">1</p>
     <p class="music-title">Around The World</p>
     <p class="music-duration">${convertToHumanTime(238733)}</p>
-=======
-      <p class="music-number">1</p>
-      <p class="music-title">Around The World</p>
-      <p class="music-duration">238733</p>
->>>>>>> d151bb1782b5ff2da6fa71d4250ac3a0c9489528
     </div>`;
 
   const markup2 = `
     <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4">
-<<<<<<< HEAD
     <p class="music-number">1</p>
     <p class="music-title">Around The World</p>
     <p class="music-duration">${convertToHumanTime(238733)}</p>
@@ -56,16 +46,6 @@ describe('AlbumTracks', () => {
     <p class="music-number">1</p>
     <p class="music-title">Around The World</p>
     <p class="music-duration">${convertToHumanTime(238733)}</p>
-=======
-      <p class="music-number">1</p>
-      <p class="music-title">Around The World</p>
-      <p class="music-duration">238733</p>
-    </div>
-    <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4">
-      <p class="music-number">1</p>
-      <p class="music-title">Around The World</p>
-      <p class="music-duration">238733</p>
->>>>>>> d151bb1782b5ff2da6fa71d4250ac3a0c9489528
     </div>`;
 
   it('should create and append the markup given a correct data', () => {
@@ -81,8 +61,5 @@ describe('AlbumTracks', () => {
 
     expect(element2.innerHTML).to.be.eql(markup2);
   });
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> d151bb1782b5ff2da6fa71d4250ac3a0c9489528
